01e7f32121fcf2cf6afab65ab6d68179a5445e26
LEZIE TRA BI <lezie01.tra@gmail.com>
2026-07-23 22:10:31 +0000

    Un produit est sensible si sa catégorie ou sous-catégorie évoque

--- src/utils/sensitiveProduct.ts
+++ src/utils/sensitiveProduct.ts
@@ -1,6 +1,10 @@
 import { Product } from "@/types/interface";
 
-const SENSITIVE_KEYWORDS = ["sexy", "lingerie"];
+const SENSITIVE_KEYWORDS = [
+    "sexy", "lingerie", "culotte",
+    "boxer", "sous-vêtement", "tanga",
+    "slip", "maillot de bain"
+];
 
 function normalize(value?: string | null): string {
     return (value || "")
